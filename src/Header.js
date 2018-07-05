f038ede242d6546a413dd3ffd1261fc0cdf8ae46
Francesca Hammond <francesca.hammond@guardian.co.uk>
2018-07-05 15:41:58 +0100

    First commit

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './FHLogo.png';
+class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+        <nav>
+          <div class="nav-wrapper">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="App-title">Frankie</span>
+  <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">JavaScript</a></li>
+  </ul>
+</div>
+</nav>
+        </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/ImageCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ImageCard extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default ImageCard;
